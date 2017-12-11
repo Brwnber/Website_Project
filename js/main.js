a04d45ca01cb602cce084092e734f94de1141670
Brwnber <kylesmall32@yahoo.com>
2017-12-10 22:36:20 -0500

    Finished Jquery

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
-var slideIndex = 0;
+//Light Switch
+$(document).ready(function(){
+    $("#lightswitch").click(function(){
+        $("body").css("background-color", "grey");
+    });
+});
 
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (let i in slides) {
-        slides[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 5000);
-}
+//text reveal
+$(document).ready(function(){
+    $(".mouseover1").mouseover(function(){
+        $(".edp").html($("<p>We specialize in start-to-finish editorial. Whether it's episodic television series, feature films, or commercials, we have talent, technicians, and technology to help tell your story.</p>"))
+    });
+    $(".mouseover1").mouseleave(function(){
+        $(".edp").html($())
+    });
+});
 
-showSlides();
+$(document).ready(function(){
+    $(".mouseover2").mouseover(function(){
+        $(".ccp").html($("<p>We provide color correcting and DI done with black magic design, to give your project the tone and look it deserves.</p>"))
+    });
+    $(".mouseover2").mouseleave(function(){
+        $(".ccp").html($())
+    });
+});
+
+$(document).ready(function(){
+    $(".mouseover3").mouseover(function(){
+        $(".adrp").html($("<p>Equipped with mixing studios, we provide ADR live recording and sound design.</p>"))
+    });
+    $(".mouseover3").mouseleave(function(){
+        $(".adrp").html($())
+    });
+});
